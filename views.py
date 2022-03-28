ea3275e10a0b96ad423ec47f0cd76ba43dabd0d1
julia <sorulaijuli@gmail.ru>
2022-03-28 22:26:27 +0300

    lesson1

---
+++ views.py
@@ -0,0 +1,13 @@
+from framework.templator import render
+
+
+class Index:
+    def __call__(self, request):
+        return '200 OK', render('index.html',
+                                object_list={'date': request.get('date', None), "title": "Онлайн-магазин фигурок"})
+
+
+class About:
+    def __call__(self, request):
+        return '200 OK', render('contact.html',
+                                object_list={'date': request.get('date', None), "title": "Контакты"})
