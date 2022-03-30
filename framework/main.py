dd0cbd7c89258f4ef09ca39266567d18c4db9a33
julia <sorulaijuli@gmail.ru>
2022-03-30 23:47:01 +0300

    lesson1

--- framework/main.py
+++ framework/main.py
@@ -1,3 +1,7 @@
+from quopri import decodestring
+from .requests import GetRequests, PostRequests
+
+
 class PageNotFound404:
     def __call__(self, request):
         return '404 WHAT', '404 PAGE Not Found'
@@ -12,13 +16,31 @@ class Framework:
         path = environ['PATH_INFO']
         if not path.endswith('/'):
             path = f'{path}/'
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = Framework.decode_value(data)
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
         for front in self.fronts_lst:
             front(request)
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
